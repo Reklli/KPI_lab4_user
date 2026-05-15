a989970e912f577dcb4235f28c90c1261e4e3a4c
Reklli <96347010+Reklli@users.noreply.github.com>
2026-05-15 15:50:59 +0300

    Add files via upload

---
+++ Services/Database.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ParkingSystemApp
+{
+    public class Database
+    {
+        public List<ParkingSpot> ParkingSpots { get; set; } = new List<ParkingSpot>();
+
+        public List<Reservation> Reservations { get; set; } = new List<Reservation>();
+
+        public List<ParkingSpot> GetAvailableSpots()
+        {
+            return ParkingSpots.Where(s => s.Status == SpotStatus.FREE).ToList();
+        }
+
+        public void SaveReservation(Reservation res)
+        {
+            Reservations.Add(res);
+        }
+
+        public void UpdateSpotStatus(string spotId, SpotStatus status)
+        {
+            var spot = ParkingSpots.FirstOrDefault(s => s.SpotId == spotId);
+            if (spot != null)
+            {
+                spot.Status = status;
+            }
+        }
+
+        public ParkingSpot? GetSpotById(string spotId)
+        {
+            return ParkingSpots.FirstOrDefault(s => s.SpotId == spotId);
+        }
+    }
+}
---
+++ Services/ParkingSystem.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ParkingSystemApp
+{
+    public class ParkingSystem
+    {
+        private Database _db;
+
+        public ParkingSystem(Database db) => _db = db;
+
+        public List<ParkingSpot> GenerateParkingMap() => _db.GetAvailableSpots();
+
+        public bool CheckSpotAvailability(ParkingSpot spot) => spot.IsAvailable();
+
+        public Reservation? ReserveSpot(ParkingSpot spot, Driver driver, int hours)
+        {
+            if (CheckSpotAvailability(spot))
+            {
+                double totalAmount = spot.HourlyRate * hours;
+
+                var payment = new Payment(new Random().Next(1000, 9999), (float)totalAmount);
+                var res = new Reservation(new Random().Next(1, 500), driver, spot, payment);
+
+                _db.SaveReservation(res);
+                spot.Status = SpotStatus.RESERVED;
+                return res;
+            }
+            return null;
+        }
+
+        public void ReleaseSpot(Driver driver)
+        {
+            if (driver.CurrentReservation != null)
+            {
+                var spot = driver.CurrentReservation.Spot;
+
+                spot.Status = SpotStatus.FREE;
+                _db.UpdateSpotStatus(spot.SpotId, SpotStatus.FREE);
+
+                driver.CurrentReservation = null;
+
+                Console.WriteLine($"[System] Reservation cleared for driver {driver.LicensePlate}.");
+            }
+        }
+    }
+}
